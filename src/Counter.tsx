771ceca68764a2af75ee73cdbffd08921a1673a7
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-07-12 17:31:53 +0900

    useState

---
+++ src/Counter.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const Counter: React.FC<{}> = () => {
+  const initialValue: any = 0;
+  const [value, setValue] = useState<number>(initialValue);
+
+  const increment = () => {
+    setValue((prevState) => prevState + 1);
+  };
+
+  const decrement = () => {
+    setValue((prevState) => prevState - 1);
+  };
+
+  return (
+    <div>
+      <div>value: {value}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+    </div>
+  );
+};
+
+export default Counter;
